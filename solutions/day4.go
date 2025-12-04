797d2388d1839e31f130ba57bb5bb61315b9afcc
justjcurtis <justjcurtis@gmail.com>
2025-12-04 16:04:35 +0000

    perf: day 4

--- solutions/day4.go
+++ solutions/day4.go
@@ -3,6 +3,7 @@ package solutions
 import (
 	"AdventOfCode2025/utils"
 	"strconv"
+	"sync"
 )
 
 func parseInputDay4(input []string) [][]rune {
@@ -13,119 +14,86 @@ func parseInputDay4(input []string) [][]rune {
 	return grid
 }
 
-func getNeighbors(x, y, width, height int) []struct{ dx, dy int } {
-	neighbors := []struct{ dx, dy int }{}
-	directions := []struct{ dx, dy int }{
-		{-1, 0}, {1, 0}, {0, -1}, {0, 1},
-		{-1, -1}, {-1, 1}, {1, -1}, {1, 1},
-	}
-	for _, d := range directions {
-		nx, ny := x+d.dx, y+d.dy
-		if nx >= 0 && nx < width && ny >= 0 && ny < height {
-			neighbors = append(neighbors, d)
-		}
-	}
+var dirs = []struct{ dx, dy int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
+
+func getNeighbors(x, y, width, height int) []struct{ x, y int } {
+	neighbors := []struct{ x, y int }{}
 	return neighbors
 }
 
-func countOccupied(grid [][]rune, x, y int) int {
-	count := 0
-	neighbors := []struct{ dx, dy int }{{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1}}
-	for _, n := range neighbors {
-		nx, ny := x+n.dx, y+n.dy
-		if ny >= 0 && ny < len(grid) && nx >= 0 && nx < len(grid[ny]) {
-			char := grid[ny][nx]
-			if char == '@' {
-				count++
-			}
-		}
-	}
-	return count
-}
-
-func solveDay4Part1(grid [][]rune) int {
-	fn := func(i int) int {
-		x, y := utils.OneDTwoD(i, len(grid[0]))
-		if grid[y][x] == '@' {
-			occupied := countOccupied(grid, x, y)
-			if occupied < 4 {
-				return 1
-			}
-		}
-		return 0
-	}
-	return utils.Parallelise(utils.IntAcc, fn, len(grid)*len(grid[0]))
-}
-
-func solveDay4Part2(grid [][]rune) int {
+func solveDay4(grid [][]rune, runForever bool) int {
 	width := len(grid[0])
 	height := len(grid)
 	totalSize := height * width
 	count := 0
-	updates := make([]int, totalSize)
-	updated := make([]int, totalSize)
-	newGrid := make([][]rune, height)
+	g := make([]byte, totalSize)
+
 	for y := 0; y < height; y++ {
-		newGrid[y] = make([]rune, width)
 		for x := 0; x < width; x++ {
-			newGrid[y][x] = grid[y][x]
+			char := grid[y][x]
+			key := utils.TwoDToOneD(x, y, width)
+			g[key] = byte(char)
 		}
 	}
 
-	for i := range updated {
-		updates[i] = -1
-	}
-	checkUpdates := false
+	var recurse func(i int) int
+
 	fn := func(i int) int {
 		var x, y int
-		if checkUpdates {
-			x, y = utils.OneDTwoD(updated[i], width)
-		} else {
-			x, y = utils.OneDTwoD(i, width)
-		}
-		char := grid[y][x]
+		x, y = utils.OneDTwoD(i, width)
+		char := g[i]
 		if char == '@' {
-			occupied := countOccupied(grid, x, y)
-			if occupied < 4 {
-				newGrid[y][x] = '.'
-				n := getNeighbors(x, y, width, height)
-				for _, neighbor := range n {
-					nx, ny := x+neighbor.dx, y+neighbor.dy
-					if grid[ny][nx] == '@' {
-						key := utils.TwoDToOneD(nx, ny, width)
-						updates[key] = key
+
+			occupied := 0
+			for _, d := range dirs {
+				nx, ny := x+d.dx, y+d.dy
+				if nx >= 0 && nx < width && ny >= 0 && ny < height {
+					nkey := utils.TwoDToOneD(nx, ny, width)
+					if g[nkey] == '@' {
+						occupied++
 					}
 				}
-				return 1
+			}
+			if occupied < 4 {
+				if !runForever {
+					return 1
+				}
+				g[i] = '.'
+				count := 1
+				for _, d := range dirs {
+					nx, ny := x+d.dx, y+d.dy
+					if nx >= 0 && nx < width && ny >= 0 && ny < height {
+						nkey := utils.TwoDToOneD(nx, ny, width)
+						count += recurse(nkey)
+					}
+				}
+				return count
 			}
 		}
 		return 0
 	}
+	recurse = fn
 
-	for {
-		newCount := utils.Parallelise(utils.IntAcc, fn, len(updated))
-		if newCount == 0 {
-			return count
-		}
-
-		filtered := []int{}
-		for _, idx := range updates {
-			if idx != -1 {
-				updates[idx] = -1
-				filtered = append(filtered, idx)
-			}
-		}
-		updated = filtered
-		grid = newGrid
-
-		checkUpdates = true
-		count += newCount
+	for i := 0; i < totalSize; i++ {
+		count += fn(i)
 	}
+	return count
 }
 
 func Day4(input []string) []string {
 	grid := parseInputDay4(input)
-	part1 := solveDay4Part1(grid)
-	part2 := solveDay4Part2(grid)
+	_ = grid
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	var part1, part2 int
+	go func() {
+		defer wg.Done()
+		part1 = solveDay4(grid, false)
+	}()
+	go func() {
+		defer wg.Done()
+		part2 = solveDay4(grid, true)
+	}()
+	wg.Wait()
 	return []string{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
